efff681f28fb078d8b27926683b54e52c41e61f7
Saisriram Gunturu <saisriram@saimail.com>
2024-03-29 14:59:09 -0400

    load and clean v1

--- scripts/load_and_clean_data.R
+++ scripts/load_and_clean_data.R
@@ -3,11 +3,17 @@
 
 library(tidyverse)
 
-loan_data <- read_csv(here::here("dataset", "loan_refusal.csv"))
+NYPD_data <- read_csv("dataset/NYPD_Shooting_Incident_Data__Historic__20240322.csv")
+
+str(NYPD_data)
+
+summary(NYPD_data)
+
 
 ## CLEAN the data
-loan_data_clean <- loan_data
+#loan_data_clean <- loan_data
 
-write_csv(loan_data_clean, file = here::here("dataset", "loan_refusal_clean.csv"))
+#write_csv(loan_data_clean, file = here::here("dataset", "loan_refusal_clean.csv"))
 
-save(loan_data_clean, file = here::here("dataset/loan_refusal.RData"))
+#saveRDS(cleaned_data, file = "cleaned_dataset.rds")
+
