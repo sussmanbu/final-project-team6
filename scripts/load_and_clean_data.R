22919b0848c7d2c30b5400c5c2d2c4d8248956f5
Saisriram Gunturu <saisriram@saimail.com>
2024-03-29 14:59:46 -0400

    load and clean -fix

--- scripts/load_and_clean_data.R
+++ scripts/load_and_clean_data.R
@@ -3,17 +3,21 @@
 
 library(tidyverse)
 
+# Read the dataset
 NYPD_data <- read_csv("dataset/NYPD_Shooting_Incident_Data__Historic__20240322.csv")
 
 str(NYPD_data)
 
+# Summary statistics
 summary(NYPD_data)
 
+NYPD_clean_data <- NYPD_data %>%
+  select(-c(OCCUR_TIME, LOC_OF_OCCUR_DESC)) %>%
+  filter(!is.na(PERP_RACE) | !is.na(VIC_RACE))
 
-## CLEAN the data
-#loan_data_clean <- loan_data
+str(NYPD_clean_data)
 
-#write_csv(loan_data_clean, file = here::here("dataset", "loan_refusal_clean.csv"))
+write_csv(NYPD_clean_data, file = here::here("dataset", "nypd_shooting_clean.csv"))
 
-#saveRDS(cleaned_data, file = "cleaned_dataset.rds")
+saveRDS(NYPD_clean_data, here::here("dataset", "nypd_shooting_clean.rds"))
 
