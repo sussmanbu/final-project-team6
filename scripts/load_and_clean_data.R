2d9a6ececc6577a47340f865734e0112923fa393
Yuqi Peng <pyq622@bu.edu>
2024-04-12 14:48:45 -0400

    load file for the new dataset

--- scripts/load_and_clean_data.R
+++ scripts/load_and_clean_data.R
@@ -5,11 +5,14 @@ library(tidyverse)
 
 # Read the dataset
 NYPD_data <- read_csv("dataset/NYPD_Shooting_Incident_Data__Historic__20240322.csv")
+NYPD_arrest_data <- read_csv("dataset/NYPD_Arrest_Data__Year_to_Date__20240410.csv")
 
 str(NYPD_data)
+str(NYPD_arrest_data)
 
 # Summary statistics
 summary(NYPD_data)
+summary(NYPD_arrest_data)
 
 NYPD_clean_data <- NYPD_data %>%
   # mutate(across(everything(), ~na_if(.x, "(null)"))) %>%
@@ -26,3 +29,4 @@ write_csv(NYPD_clean_data, file = here::here("dataset", "nypd_shooting_clean.csv
 
 saveRDS(NYPD_clean_data, here::here("dataset", "nypd_shooting_clean.rds"))
 
+saveRDS(NYPD_arrest_data, here::here("dataset", "nypd_arrest.rds"))
